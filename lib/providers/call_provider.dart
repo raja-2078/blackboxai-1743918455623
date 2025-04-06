cbb86cf70c385a5d9d0e34040e84383cea0e9034
raja-2078 <rajanismilesalways@gmail.com>
2025-04-06 11:18:00 +0530

    Add lib/providers/call_provider.dart 1743918480545

---
+++ lib/providers/call_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CallState {
+  final String proficiencyLevel;
+  final String genderPreference;
+
+  CallState({
+    this.proficiencyLevel = '',
+    this.genderPreference = '',
+  });
+
+  CallState copyWith({
+    String? proficiencyLevel,
+    String? genderPreference,
+  }) {
+    return CallState(
+      proficiencyLevel: proficiencyLevel ?? this.proficiencyLevel,
+      genderPreference: genderPreference ?? this.genderPreference,
+    );
+  }
+}
+
+class CallNotifier extends StateNotifier<CallState> {
+  CallNotifier() : super(CallState());
+
+  void setProficiencyLevel(String level) {
+    state = state.copyWith(proficiencyLevel: level);
+  }
+
+  void setGenderPreference(String gender) {
+    state = state.copyWith(genderPreference: gender);
+  }
+
+  void reset() {
+    state = CallState();
+  }
+}
+
+final callProvider = StateNotifierProvider<CallNotifier, CallState>((ref) {
+  return CallNotifier();
+});
