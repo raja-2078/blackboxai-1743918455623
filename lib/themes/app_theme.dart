70638f14df9b1856cbe0bae65303d4ce68d0100d
raja-2078 <rajanismilesalways@gmail.com>
2025-04-06 12:42:44 +0530

    Add lib/themes/app_theme.dart 1743923564938

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final themeProvider = StateProvider<String>((ref) => 'System Default');
 
 class AppTheme {
+  static ThemeData getTheme(String themeMode) {
+    switch (themeMode) {
+      case 'Light':
+        return lightTheme;
+      case 'Dark':
+        return darkTheme;
+      default:
+        return ThemeData.light();
+    }
+  }
+
   static final lightTheme = ThemeData(
     colorScheme: ColorScheme.light(
       primary: Colors.blue.shade800,
       secondary: Colors.orange.shade600,
-      background: Colors.grey.shade100,
+      surface: Colors.grey.shade100,
     ),
     useMaterial3: true,
   );
@@ -14,7 +28,7 @@ class AppTheme {
     colorScheme: ColorScheme.dark(
       primary: Colors.blue.shade300,
       secondary: Colors.orange.shade400,
-      background: Colors.grey.shade900,
+      surface: Colors.grey.shade900,
     ),
     useMaterial3: true,
   );
