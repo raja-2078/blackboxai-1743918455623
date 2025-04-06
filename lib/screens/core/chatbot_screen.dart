e546295d5abcd51cca12a711256d2f23cfe2e770
raja-2078 <rajanismilesalways@gmail.com>
2025-04-06 11:17:51 +0530

    Add lib/screens/core/chatbot_screen.dart 1743918471699

---
+++ lib/screens/core/chatbot_screen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../providers/chat_provider.dart';
+import '../../../services/rasa_service.dart';
+
+class ChatbotScreen extends ConsumerStatefulWidget {
+  const ChatbotScreen({super.key});
+
+  @override
+  ConsumerState<ChatbotScreen> createState() => _ChatbotScreenState();
+}
+
+class _ChatbotScreenState extends ConsumerState<ChatbotScreen> {
+  final _messageController = TextEditingController();
+  final _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _sendMessage() async {
+    if (_messageController.text.isEmpty) return;
+
+    final message = _messageController.text;
+    _messageController.clear();
+
+    // Add user message to chat
+    ref.read(chatProvider.notifier).addMessage(
+          sender: 'user',
+          text: message,
+        );
+
+    // Scroll to bottom
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
+
+    // Get bot response
+    try {
+      final response = await ref.read(rasaServiceProvider).sendMessage(message);
+      ref.read(chatProvider.notifier).addMessage(
+            sender: 'bot',
+            text: response,
+          );
+    } catch (e) {
+      ref.read(chatProvider.notifier).addMessage(
+            sender: 'bot',
+            text: 'Sorry, I encountered an error. Please try again.',
+          );
+    }
+
+    // Scroll to bottom again after bot response
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final chatState = ref.watch(chatProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('English Practice Chatbot'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(16),
+              itemCount: chatState.messages.length,
+              itemBuilder: (context, index) {
+                final message = chatState.messages[index];
+                return Align(
+                  alignment: message.sender == 'user'
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 4),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: message.sender == 'user'
+                          ? Theme.of(context).colorScheme.primary
+                          : Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Text(
+                      message.text,
+                      style: TextStyle(
+                        color: message.sender == 'user'
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: 'Type your message...',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 12,
+                      ),
+                    ),
+                    onSubmitted: (_) => _sendMessage(),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
